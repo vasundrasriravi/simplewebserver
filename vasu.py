45b16bd939d40bda7d8b826e4482fa8ba96e7bbb
vasundra <vasundrasri49@gmail.com>
2022-12-19 05:54:20 +0000

    success vasundra

--- vasu.py
+++ vasu.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -39,11 +21,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-![OUTPUT](./image/took.png)
-
-## RESULT:
-this program has been executed successfully
